6709d663ce2846c909942600aeea613d75b39c63
lumiaqian <cjj1@truesightai.com>
2026-09-24 22:33:35 +0800

    fix(css): 修复 Dev Lab 样式被包裹在 media query 内导致的未渲染问题，重构精修控制台视觉质感

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const SHELL_CACHE = "firecloud-shell-v37";
+const SHELL_CACHE = "firecloud-shell-v38";
 const SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=37",
+  "./css/style.css?v=38",
   "./js/forecast.mjs?v=6",
   "./js/api.mjs?v=7",
   "./js/weather-fx.mjs?v=8",
